63b40fd04b50080915445313b9c9fe9fb1323a19
Agustín <196411814+snowyyd@users.noreply.github.com>
2025-04-01 11:49:46 -0300

    refactor: builkd-containers script

--- build-containers.sh
+++ build-containers.sh
@@ -12,7 +12,22 @@ echo "Working dir: ${G_CONTAINERS_DIR}"
 
 # TODO: check if ${G_CONTAINERS_DIR} exists
 
-echo "Building Docker images..."
-docker build -t godot-fedora:latest -f ${G_CONTAINERS_DIR}/Dockerfile.base ${G_CONTAINERS_DIR} &
-docker build --build-arg="img_version=latest" -t godot-windows:latest -f ${G_CONTAINERS_DIR}/Dockerfile.windows ${G_CONTAINERS_DIR} &
-wait
+if [ -z "$1" ]; then
+echo "Usage: $0 <container> (Possible values: base, windows, linux, web, osx, android, ios)"
+exit 1
+fi
+
+buildImage()
+{
+  echo "Docker target: $1"
+  if [ "$1" == "base" ]; then
+    docker build -t godot-fedora:latest -f ${G_CONTAINERS_DIR}/Dockerfile.base ${G_CONTAINERS_DIR}
+  else
+    docker build --build-arg="img_version=latest" -t godot-$1:latest -f ${G_CONTAINERS_DIR}/Dockerfile.$1 ${G_CONTAINERS_DIR}
+  fi
+}
+
+case "$1" in
+  base|windows|linux|web|osx|android|ios|xcode) buildImage "$1";;
+  *) echo "Error: Invalid target."; exit 1;;
+esac
